76ce77243bd28ed6d149a00fe7d5f2cada6c8a09
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-27 16:41:06 +0600

    did make a post request on sign up form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import HeaderBanner from "./components/HeaderBanner/HeaderBanner";
+import Products from "./components/Products/Products";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
+import AddProduct from "./components/AddProduct/AddProduct";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+          <Switch>
+              <PrivateRoute path="/home">
+                  <Navbar />
+                  <HeaderBanner />
+                  <AddProduct />
+                  <Products />
+                  <Footer />
+              </PrivateRoute>
+              <Route exact path="/">
+                <Navbar />
+                  <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Navbar />
+                  <Login />
+              </Route>
+          </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/localImages.js
@@ -0,0 +1,7 @@
+const localImage = {
+    bannerImg1: require('../images/banner1.jpg').default,
+    bannerImg2: require('../images/banner2.png').default,
+    bannerImg3: require('../images/banner3.jpg').default
+}
+
+export default localImage;
